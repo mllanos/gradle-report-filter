dcd6600766e6d941a453e52c5f3ce4374c2d8bf1
mllanos <mario.llanos@appdirect.com>
2021-03-09 18:25:59 -0300

    Working filter.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,47 @@
 use std::env;
-use std::fs;
+use std::io::{BufRead, BufReader};
 use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
+use std::collections::VecDeque;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
     let filename = &args[1];
     let query = &args[2];
+    let mut i = 0;
+    let mut matches = VecDeque::new();
 
     println!("Searching for [{}] in {}", query, filename);
+    
+    let file = BufReader::new(File::open(filename).unwrap());
+    let mut lines: Vec<_> = file.lines().map(|line| { line.unwrap() }).collect();
+    lines.reverse();
 
-    if let Ok(lines) = read_lines(filename) {
-        for line in lines {
-            if let Ok(l) = line {
-                if l.contains(query) {
-                    println!("{}", l);
+    for line in lines.clone() {
+        if line.contains("+") || line.contains("\\") {
+            if line.contains(query) {
+                let mut level = line.matches("    ").count();
+                matches.push_front(line);
+                loop {
+                    for _line in lines.clone().drain(i + 1..lines.len()) {        
+                        let _level = _line.matches("    ").count();
+                        if _level == level - 1 {
+                            matches.push_front(_line);
+                            level = _level;
+                            if level == 0 {
+                                break;
+                            }
+                        }
+                    }
+                    if level == 0 {
+                        break;
+                    }
                 }
             }
-        }    
+        }
+        i = i + 1;
+    }
+
+    for m in matches.iter() {
+        println!("{}", m);
     }
 }
